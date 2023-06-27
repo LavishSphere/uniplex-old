a7242bd44877718462dd6a06b8a0f06e418f8605
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-06-26 20:25:01 -0700

    Update page.js

--- app/policy/page.js
+++ app/policy/page.js
@@ -1,7 +1,3 @@
-import React from 'react';
-
-const TermsPage = () => {
-  return (
 <style>
   [data-custom-class='body'], [data-custom-class='body'] * {
           background: transparent !important;
@@ -144,5 +140,3 @@ Calibri;color:#595959;mso-themecolor:text1;mso-themetint:166;"><strong><bdt clas
       <div style="color: #595959;font-size: 14px;font-family: Arial;padding-top:16px;">
       These terms of use were created using Termly's <a style="color: rgb(48, 48, 241) !important;" href="https://termly.io/products/terms-and-conditions-generator/">Terms and Conditions Generator</a>.
       </div>
-
-export default TermsPage;
