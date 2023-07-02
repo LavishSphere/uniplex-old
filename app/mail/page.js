0ce9377b62e034e045f7dce05a34f92ddb8ddeb5
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-07-01 18:21:13 -0700

    Update page.js

--- app/mail/page.js
+++ app/mail/page.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-const RedirectPage = () => {
-  return (
-    <div>
-      Redirecting...
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.location.href = 'https://coldkigbjjv.typeform.com/to/Q26PaNkG';
-          `,
-        }}
-      />
-    </div>
-  );
-};
+class EmbedPage extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = '//embed.typeform.com/next/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
 
-export default RedirectPage;
+  componentWillUnmount() {
+    const script = document.querySelector('script[src="//embed.typeform.com/next/embed.js"]');
+    document.body.removeChild(script);
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          data-tf-widget="Q26PaNkG"
+          data-tf-opacity="100"
+          data-tf-iframe-props="title=Uniplex Mailing List Form"
+          data-tf-transitive-search-params
+          data-tf-medium="snippet"
+          style={{ width: '100%', height: '500px' }}
+        />
+      </div>
+    );
+  }
+}
+
+export default EmbedPage;
