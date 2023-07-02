d3cf0701757b093aae047568e529de2df5ec2272
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-07-01 19:26:59 -0700

    Update page.js

--- app/policy/page.js
+++ app/policy/page.js
@@ -1,85 +1,103 @@
 import React from 'react';
 
 const PrivacyPolicy = () => {
-  return (
-    <div>
-      <h1>Privacy Policy</h1>
+  return React.createElement(
+    'div',
+    null,
+    React.createElement('h1', null, 'Privacy Policy'),
 
-      <p>
-        This Privacy Policy governs the manner in which Uniplex Incorporated (&quot;Uniplex,&quot; &quot;we,&quot; &quot;us,&quot; or &quot;our&quot;) collects, uses, maintains, and discloses information collected from users (each, a &quot;User&quot;) of the https://uniplex.xyz website (the &quot;Website&quot;) and all related services provided by Uniplex (collectively, the &quot;Services&quot;). This Privacy Policy applies to the Website and all products and services offered by Uniplex.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'This Privacy Policy governs the manner in which Uniplex Incorporated ("Uniplex," "we," "us," or "our") collects, uses, maintains, and discloses information collected from users (each, a "User") of the https://uniplex.xyz website (the "Website") and all related services provided by Uniplex (collectively, the "Services"). This Privacy Policy applies to the Website and all products and services offered by Uniplex.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>Information We Collect</h2>
+    React.createElement('h2', null, 'Information We Collect'),
 
-      <p>
-        We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our Website, register on the Website, place an order, fill out a form, respond to a survey, and in connection with other activities, services, features, or resources we make available on our Website. Users may be asked for, as appropriate, name, email address, mailing address, phone number, and payment information. Users may, however, visit our Website anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain Website-related activities.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our Website, register on the Website, place an order, fill out a form, respond to a survey, and in connection with other activities, services, features, or resources we make available on our Website. Users may be asked for, as appropriate, name, email address, mailing address, phone number, and payment information. Users may, however, visit our Website anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain Website-related activities.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>How We Use Collected Information</h2>
+    React.createElement('h2', null, 'How We Use Collected Information'),
 
-      <p>
-        Uniplex may collect and use Users&apos; personal information for the following purposes:
-      </p>
+    React.createElement(
+      'p',
+      null,
+      "Uniplex may collect and use Users' personal information for the following purposes:"
+    ),
 
-      <ul>
-        <li>To improve customer service: Information you provide helps us respond to your customer service requests and support needs more efficiently.</li>
-        <li>To personalize user experience: We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Website.</li>
-        <li>To improve our Website: We may use feedback you provide to improve our products and services.</li>
-        <li>To process payments: We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.</li>
-        <li>To run a promotion, contest, survey, or other Website feature: To send Users information they agreed to receive about topics we think will be of interest to them.</li>
-        <li>To send periodic emails: We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests.</li>
-      </ul>
+    React.createElement(
+      'ul',
+      null,
+      React.createElement('li', null, 'To improve customer service: Information you provide helps us respond to your customer service requests and support needs more efficiently.'),
+      React.createElement('li', null, 'To personalize user experience: We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Website.'),
+      React.createElement('li', null, 'To improve our Website: We may use feedback you provide to improve our products and services.'),
+      React.createElement('li', null, 'To process payments: We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.'),
+      React.createElement('li', null, 'To run a promotion, contest, survey, or other Website feature: To send Users information they agreed to receive about topics we think will be of interest to them.'),
+      React.createElement('li', null, 'To send periodic emails: We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests.')
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>How We Protect Your Information</h2>
+    React.createElement('h2', null, 'How We Protect Your Information'),
 
-      <p>
-        We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our Website.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our Website.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>Sharing Your Personal Information</h2>
+    React.createElement('h2', null, 'Sharing Your Personal Information'),
 
-      <p>
-        We do not sell, trade, or rent Users&apos; personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers for the purposes outlined above.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      "We do not sell, trade, or rent Users' personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers for the purposes outlined above."
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>Changes to This Privacy Policy</h2>
+    React.createElement('h2', null, 'Changes to This Privacy Policy'),
 
-      <p>
-        Uniplex has the discretion to update this Privacy Policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this Privacy Policy periodically and become aware of modifications.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'Uniplex has the discretion to update this Privacy Policy at any time. When we do, we will revise the updated date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this Privacy Policy periodically and become aware of modifications.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>Your Acceptance of These Terms</h2>
+    React.createElement('h2', null, 'Your Acceptance of These Terms'),
 
-      <p>
-        By using this Website, you signify your acceptance of this Privacy Policy. If you do not agree to this Privacy Policy, please do not use our Website. Your continued use of the Website following the posting of changes to this Privacy Policy will be deemed your acceptance of those changes.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'By using this Website, you signify your acceptance of this Privacy Policy. If you do not agree to this Privacy Policy, please do not use our Website. Your continued use of the Website following the posting of changes to this Privacy Policy will be deemed your acceptance of those changes.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <h2>Contacting Us</h2>
+    React.createElement('h2', null, 'Contacting Us'),
 
-      <p>
-        If you have any questions about this Privacy Policy, the practices of this Website, or your dealings with this Website, please contact us at <a href=&quot;mailto:website@uniplex.xyz&quot;>website@uniplex.xyz</a>.
-      </p>
+    React.createElement(
+      'p',
+      null,
+      'If you have any questions about this Privacy Policy, the practices of this Website, or your dealings with this Website, please contact us at ',
+      React.createElement('a', { href: 'mailto:website@uniplex.xyz' }, 'website@uniplex.xyz'),
+      '.'
+    ),
 
-      <hr />
+    React.createElement('hr', null),
 
-      <p>
-        This document was last updated on June 26, 2023.
-      </p>
-    </div>
+    React.createElement('p', null, 'This document was last updated on June 26, 2023.')
   );
 };
 
