7a4f2ba1634c06cc5c5086835cb2cb985d11344e
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-07-01 18:56:26 -0700

    Update page.js

--- app/terms/page.js
+++ app/terms/page.js
@@ -2,44 +2,44 @@ import React from 'react';
 
 const TermsPage = () => {
   return (
-    <div>
-      <h1>Terms and Conditions of Service for Uniplex Incorporated</h1>
-      <p>Last updated: June 26, 2023</p>
-      <p>Welcome to Uniplex Incorporated (&quot;Uniplex,&quot; &quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). These Terms and Conditions (the &quot;Terms&quot;) govern your access to and use of the website https://uniplex.xyz (the &quot;Website&quot;) and all related services provided by Uniplex, including but not limited to our Minecraft server services (collectively, the &quot;Services&quot;). Please read these Terms carefully before using our Website or accessing our Services.</p>
+    React.createElement('div', null,
+      React.createElement('h1', null, 'Terms and Conditions of Service for Uniplex Incorporated'),
+      React.createElement('p', null, 'Last updated: June 26, 2023'),
+      React.createElement('p', null, 'Welcome to Uniplex Incorporated ("Uniplex," "we," "us," or "our"). These Terms and Conditions (the "Terms") govern your access to and use of the website https://uniplex.xyz (the "Website") and all related services provided by Uniplex, including but not limited to our Minecraft server services (collectively, the "Services"). Please read these Terms carefully before using our Website or accessing our Services.'),
 
-      <h2>1. Acceptance of Terms</h2>
-      <p>By accessing the Website or using our Services, you agree to be bound by these Terms. If you do not agree with any part of these Terms, you must not use our Website or Services.</p>
+      React.createElement('h2', null, '1. Acceptance of Terms'),
+      React.createElement('p', null, 'By accessing the Website or using our Services, you agree to be bound by these Terms. If you do not agree with any part of these Terms, you must not use our Website or Services.'),
 
-      <h2>2. Eligibility</h2>
-      <p>2.1. You must be at least 13 years old to use our Services. By using our Services, you represent and warrant that you are at least 13 years old. If you are under the age of 18, you must obtain parental consent to use our Services.</p>
+      React.createElement('h2', null, '2. Eligibility'),
+      React.createElement('p', null, '2.1. You must be at least 13 years old to use our Services. By using our Services, you represent and warrant that you are at least 13 years old. If you are under the age of 18, you must obtain parental consent to use our Services.'),
 
-      <h2>3. Minecraft Server Services</h2>
-      <p>3.1. Uniplex provides Minecraft server services for entertainment purposes only. You agree to use the Minecraft server and associated features in compliance with our rules and guidelines, as provided on the Website or within the game.</p>
-      <p>3.2. Uniplex reserves the right to suspend or terminate your access to the Minecraft server or any related Services at any time for violating these Terms or our rules and guidelines.</p>
+      React.createElement('h2', null, '3. Minecraft Server Services'),
+      React.createElement('p', null, '3.1. Uniplex provides Minecraft server services for entertainment purposes only. You agree to use the Minecraft server and associated features in compliance with our rules and guidelines, as provided on the Website or within the game.'),
+      React.createElement('p', null, '3.2. Uniplex reserves the right to suspend or terminate your access to the Minecraft server or any related Services at any time for violating these Terms or our rules and guidelines.'),
 
-      <h2>4. Payment and Billing</h2>
-      <p>4.1. Uniplex accepts payments through PayPal, Credit Cards, Debit Cards, Bank transfers, and Cryptocurrencies. By making a payment, you authorize Uniplex to charge the specified amount to your selected payment method.</p>
-      <p>4.2. All payments are non-refundable unless otherwise specified or required by applicable law.</p>
-      <p>4.3. In the event of a payment dispute or chargeback, Uniplex reserves the right to suspend or terminate your access to our Services until the dispute is resolved.</p>
+      React.createElement('h2', null, '4. Payment and Billing'),
+      React.createElement('p', null, '4.1. Uniplex accepts payments through PayPal, Credit Cards, Debit Cards, Bank transfers, and Cryptocurrencies. By making a payment, you authorize Uniplex to charge the specified amount to your selected payment method.'),
+      React.createElement('p', null, '4.2. All payments are non-refundable unless otherwise specified or required by applicable law.'),
+      React.createElement('p', null, '4.3. In the event of a payment dispute or chargeback, Uniplex reserves the right to suspend or terminate your access to our Services until the dispute is resolved.'),
 
-      <h2>5. Privacy</h2>
-      <p>Your privacy is important to us. Please review our <a href=&quot;https://uniplex.xyz/privacy-policy&quot; target=&quot;_blank&quot; rel=&quot;noopener noreferrer&quot;>Privacy Policy</a> to understand how we collect, use, and disclose information related to your use of our Services.</p>
+      React.createElement('h2', null, '5. Privacy'),
+      React.createElement('p', null, 'Your privacy is important to us. Please review our Privacy Policy, available on the Website, to understand how we collect, use, and disclose information related to your use of our Services.'),
 
-      <h2>6. Intellectual Property</h2>
-      <p>6.1. All content, materials, and intellectual property rights on the Website and within our Services are owned by or licensed to Uniplex. You agree not to reproduce, distribute, modify, or create derivative works of any content or materials without our prior written consent.</p>
-      <p>6.2. Minecraft is a registered trademark of Mojang AB. Uniplex is not affiliated with or endorsed by Mojang AB.</p>
+      React.createElement('h2', null, '6. Intellectual Property'),
+      React.createElement('p', null, '6.1. All content, materials, and intellectual property rights on the Website and within our Services are owned by or licensed to Uniplex. You agree not to reproduce, distribute, modify, or create derivative works of any content or materials without our prior written consent.'),
+      React.createElement('p', null, '6.2. Minecraft is a registered trademark of Mojang AB. Uniplex is not affiliated with or endorsed by Mojang AB.'),
 
-      <h2>7. Limitation of Liability</h2>
-      <p>7.1. To the maximum extent permitted by law, Uniplex and its affiliates shall not be liable for any direct, indirect, incidental, consequential, or exemplary damages arising from your use of our Website or Services, including but not limited to damages for loss of profits, data, or other intangible losses.</p>
+      React.createElement('h2', null, '7. Limitation of Liability'),
+      React.createElement('p', null, '7.1. To the maximum extent permitted by law, Uniplex and its affiliates shall not be liable for any direct, indirect, incidental, consequential, or exemplary damages arising from your use of our Website or Services, including but not limited to damages for loss of profits, data, or other intangible losses.'),
 
-      <h2>8. Modification and Termination</h2>
-      <p>8.1. Uniplex reserves the right to modify or update these Terms at any time without prior notice. Continued use of our Website or Services after any such changes shall constitute your consent to such changes.</p>
-      <p>8.2. We may also terminate or suspend your access to our Services at any time without prior notice or liability, for any reason including, but not limited to, a violation of these Terms.</p>
+      React.createElement('h2', null, '8. Modification and Termination'),
+      React.createElement('p', null, '8.1. Uniplex reserves the right to modify or update these Terms at any time without prior notice. Continued use of our Website or Services after any such changes shall constitute your consent to such changes.'),
+      React.createElement('p', null, '8.2. We may also terminate or suspend your access to our Services at any time without prior notice or liability, for any reason including, but not limited to, a violation of these Terms.'),
 
-      <p>By using our Services, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions. If you have any questions or concerns, please contact us at <a href=&quot;mailto:website@uniplex.xyz&quot;>website@uniplex.xyz</a>.</p>
-
-      <p>Uniplex Incorporated is located in California, United States, Los Angeles County.</p>
-    </div>
+      React.createElement('p', null, 'By using our Services, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions. If you have any questions or concerns, please contact us at '),
+      React.createElement('a', { href: 'mailto:website@uniplex.xyz' }, 'website@uniplex.xyz'),
+      React.createElement('p', null, 'Uniplex Incorporated is located in California, United States, Los Angeles County.')
+    )
   );
 };
 
