db6240ce283782c22bb50343bae7f81483c9797b
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-07-01 18:24:25 -0700

    Update page.js

--- app/mail/page.js
+++ app/mail/page.js
@@ -1,29 +1,18 @@
 import React from 'react';
 
-const EmbedPage = () => {
-  React.useLayoutEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//embed.typeform.com/next/embed.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
+const RedirectPage = () => {
   return (
     <div>
-      <div
-        data-tf-widget="Q26PaNkG"
-        data-tf-opacity="100"
-        data-tf-iframe-props="title=Uniplex Mailing List Form"
-        data-tf-transitive-search-params
-        data-tf-medium="snippet"
-        style={{ width: '100%', height: '500px' }}
+      Redirecting...
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.location.href = 'https://coldkigbjjv.typeform.com/to/Q26PaNkG';
+          `,
+        }}
       />
     </div>
   );
 };
 
-export default EmbedPage;
+export default RedirectPage;
