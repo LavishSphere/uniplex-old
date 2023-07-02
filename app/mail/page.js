c1f081c7c48b5d144636ae83bd1e42554ea6892a
Khayrul, Ashsmith <133931752+LavishSphere@users.noreply.github.com>
2023-07-01 18:22:48 -0700

    Update page.js

--- app/mail/page.js
+++ app/mail/page.js
@@ -1,32 +1,29 @@
 import React from 'react';
 
-class EmbedPage extends React.Component {
-  componentDidMount() {
+const EmbedPage = () => {
+  React.useLayoutEffect(() => {
     const script = document.createElement('script');
     script.src = '//embed.typeform.com/next/embed.js';
     script.async = true;
     document.body.appendChild(script);
-  }
 
-  componentWillUnmount() {
-    const script = document.querySelector('script[src="//embed.typeform.com/next/embed.js"]');
-    document.body.removeChild(script);
-  }
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <div
-          data-tf-widget="Q26PaNkG"
-          data-tf-opacity="100"
-          data-tf-iframe-props="title=Uniplex Mailing List Form"
-          data-tf-transitive-search-params
-          data-tf-medium="snippet"
-          style={{ width: '100%', height: '500px' }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <div
+        data-tf-widget="Q26PaNkG"
+        data-tf-opacity="100"
+        data-tf-iframe-props="title=Uniplex Mailing List Form"
+        data-tf-transitive-search-params
+        data-tf-medium="snippet"
+        style={{ width: '100%', height: '500px' }}
+      />
+    </div>
+  );
+};
 
 export default EmbedPage;
